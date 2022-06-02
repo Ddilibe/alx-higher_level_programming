89c3461d5950e0537c60cba54bd49d015428bcce
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-02 22:13:20 +0100

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 import sys
 w = 0
 for i in range(1, len(sys.argv)):
-    if '.' in str(sys.argv[i]):
-        j = float(sys.argv[i])
-    else:
-        j = int(sys.argv[i])
+    j = int(sys.argv[i])
     w += j
-print(w)
+if __name__ == '__main__':
+    print(w)
