157ecefaae9ac7fdfe27176136ceb4b1ca318558
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-02 22:04:55 +0100

    hoe=w to make a script  dynamic

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-calculator_1
-add = __import__('calculator_1').add
-sub = __import__('calculator_1').sub
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 j = len(sys.argv) - 1
-if j == 1:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-elif j > 0:
-    print("{} arguments: ".format(j))
-    for i in range(1,len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("0 arguments.")
+if __name__ == '__main__':
+    if j == 1:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    elif j > 0:
+        print("{} arguments: ".format(j))
+        for i in range(1,len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
+        else:
+            print("0 arguments.")
 
