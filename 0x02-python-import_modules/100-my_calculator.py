5ecb1ffce66f29881a57dcb631208e601a70e95b
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-04 08:30:18 +0100

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, div, mul
-import sys
 if __name__ == '__main__':
+    from calculator_1 import add, sub, div, mul
+    import sys
     if len(sys.argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b> \n1')
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
     else:
         a = int(sys.argv[1])
         s = sys.argv[2]
         b = int(sys.argv[3])
         if s not in ('+', '-', '*', '/'):
-            print('Unknown operator. Avaliable operators: +, -, * and /\n1')
+            print('Unknown operator. Avaliable operators: +, -, * and /')
+            sys.exit(1)
         else:
             if s == '+':
-                print("{} + {} = {}\n0".format(a, b, add(a, b)))
+                print("{} + {} = {}".format(a, b, add(a, b)))
             elif s == '-':
-                print("{} - {} = {}\n0".format(a, b, sub(a, b)))
+                print("{} - {} = {}".format(a, b, sub(a, b)))
             elif s == '*':
-                print("{} * {} = {}\n0".format(a, b, mul(a, b)))
+                print("{} * {} = {}".format(a, b, mul(a, b)))
             else:
-                print("{} / {} = {}\n0".format(a, b, div(a, b)))
+                print("{} / {} = {}".format(a, b, div(a, b)))
+            sys.exit(0)
