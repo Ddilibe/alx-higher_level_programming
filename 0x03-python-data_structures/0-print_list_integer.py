5c5409a914a8a4effa024b0bb5b12882c03a05e3
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-06 21:08:23 +0100

    [featured]: Added a posibility for an empty list potential in a few program

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if len(my_list) != 0:
-        for i in my_list:
-            print("{}".format(i))
-    else:
-        print(" ")
+    i = 0
+    while i < len(my_list):
+        print("{}".format(my_list[i]))
+        i += 1
