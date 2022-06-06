b4d6f4bbf7c3f5a14544eb22d41c9ae89ae80a4d
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-06 21:32:17 +0100

    [feature]: Picking out elements in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if not(idx >= 0):
+    if (idx < 0):
         return None
     else:
-        if not((idx ) < len(my_list)):
+        if (idx ) < (len(my_list) + 1):
             return None
         else:
-            return my_list[idx]
+            return (my_list[idx])
