c4ae3868e1103dfe407365635b450a8af7e6a02e
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-06 22:03:40 +0100

    [feature]: Getting flexible with tuples immutability

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 1:
-        tuple_a = change_one(tuple_a)
-    if len(tuple_b) == 1:
-        tuple_b = change_one(tuple_b)
-    if len(tuple_a) == 0:
-        tuple_a = change_two(tuple_a)
-    if len(tuple_b) == 0:
-        tuple_b = change_two(tuple_b)
-    f = []
-    for i in range(0, 2):
-        f.append(tuple_a[i] + tuple_b[i])
-    return (tuple(f))
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
 
-def change_one(tupl):
-    tupl = list(tupl)
-    tupl.append(0)
-    tupl = tuple(tupl)
-    return (tupl)
+    if len_a == 0:
+        a1 = 0
+        a2 = 0
+    elif len_a == 1:
+        a1 = tuple_a[0]
+        a2 = 0
+    else:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
 
-def change_two(tupl):
-    tupl = list(tupl)
-    for i in range(0,2):
-        tupl.append(0)
-    tupl = tuple(tupl)
-    return (tupl)
+    if len_b == 0:
+        b1 = 0
+        b2 = 0
+    elif len_b == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    else:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+
+    new_tuple = (a1 + b1, a2 + b2)
+
+    return (new_tuple)
