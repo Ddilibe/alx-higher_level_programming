60d50df91bba3a946ec867a6cf9c05799c2da8dc
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-06 21:43:16 +0100

    [feature]: Adding string format to print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         if type(i) == list:
             for h in i:
-                print("{}".format(h), end=" ")
+                print("{:d}".format(h), end=" ")
         print("\n")
