807cc1b27047f66f2b9e31a12cb44df6c96bd4c5
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-08 16:04:43 +0100

    [Feature]: Set Union and Intersection

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    first =  [i for i in set_1 if i not in set_2]
-    second = [i for i in set_2 if i not in set_1]
-    return (list(set(first + second)))
+    return (set_1 ^ set_2)
