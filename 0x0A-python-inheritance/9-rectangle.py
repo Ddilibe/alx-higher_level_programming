65e361a44441d0cd9750a3c6da1276e5e8860205
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-07-05 00:31:47 +0100

    [Feature]: Still correcting some errors I made

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -13,10 +13,10 @@ class Rectangle(BaseGeometry):
     """
     def __init__(self, width, height):
         """ A class that initializes the rectangle class"""
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
     def area(self):
         """ Method for calculating the area of the shape"""
@@ -24,4 +24,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         de = "[Rectangle] "
-        return (de + str(self.__width) + "/" + str(self.__height))
+        return ("{} {}/{}".format(de, self.__width, self.__height))
