6085490f68a57648e5a20b1de02eba3880b4b4e1
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-07-05 00:35:10 +0100

    [Feature]: completed the python error checks

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,6 +6,7 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """
         A square class defined using a square class
@@ -15,7 +16,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-    
+
     def __str__(self):
         """ Method that describes a class """
         return ("[Square] {}/{}".format(self.__size, self.__size))
