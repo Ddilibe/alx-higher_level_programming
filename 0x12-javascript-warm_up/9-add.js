07298a4b3a9eca102a93be8caecbe285409eb47a
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-06 04:17:03 +0100

    [Update]: Modified myVar  in a file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-	return (Number.parseInt(a)+Number.parseInt(b));
+function add (a, b) {
+  return (Number.parseInt(a) + Number.parseInt(b));
 }
-const james = process.argv
+const james = process.argv;
 console.log(add(james[2], james[3]));
