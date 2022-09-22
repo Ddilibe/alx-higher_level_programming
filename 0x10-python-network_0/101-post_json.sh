5499631707550bf3aeea06566af9e9180329a26e
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-22 16:33:50 +0100

    [Fix]: Fixing locally

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that sends a Json POst request to a URL passed as
 # the first argument an d@$2" -H "Content-Type: application/json"e
-curl -s "$1" -X POST -d "$2"
+curl -s "$1" -X POST -d "@$2" -H "Content-Type: application/json"
