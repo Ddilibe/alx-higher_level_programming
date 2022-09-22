efaa47b4f10b009643d9c994b14c057357ec61a5
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-22 16:52:29 +0100

    [Fix]: Created a solution

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes
 # the server to respond with a message containing "You got me!"
-curl -sLi 0.0.0.0:5000/catch_me -X PUT -F "user_id=98"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -F "user_id=98"
