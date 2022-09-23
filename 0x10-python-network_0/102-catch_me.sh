acaa174be2f762570bda73ca71741533306f6765
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-22 20:48:27 -0700

    [Changed a few things but it not working

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes
 # the server to respond with a message containing "You got me!"
-curl -sL 0.0.0.0:5000/catch_me_2 -X PUT -F "user_id=98"
+curl -s 0.0.0.0:5000/catch_me -X PUT -L -F "user_id=98" -H "Origin: HolbertonSchool"
