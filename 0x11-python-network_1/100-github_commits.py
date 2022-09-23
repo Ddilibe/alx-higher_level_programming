076517add84d5ffb86065169c9c3184f204641b4
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-09-23 16:37:58 +0100

    [Fix]: Put the answer in reversed mode

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     import json
 
     url = "https://api.github.com/repos/{}/{}/commits".\
-        format(sys.argv[1], sys.argv[2])
+        format(sys.argv[2], sys.argv[1])
     req = requests.get(url)
     jam = req.json()
     jam = jam[::-1]
