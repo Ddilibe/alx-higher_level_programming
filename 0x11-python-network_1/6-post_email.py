c370b83b547f897ff9af109ae3e06fc244450978
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-09-23 13:49:01 +0100

    [Update]: Making post request with requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Script that takes in a url and an email address, sends apost request
+    with the passed url with the email as a parameter and finally displays
+    the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests, sys
+    data = {"email":sys.argv[2]}
+    got = requests.post(sys.argv[1], data=data)
+    print("Your email is: {}".format(got.text['form']['email']))
