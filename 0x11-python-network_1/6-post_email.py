203878e390752705f6b449639e00c7793108f491
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-23 17:18:41 -0700

    [Fix]: Don't know the error but still trying to fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-    Script that takes in a url and an email address, sends apost request
-    with the passed url with the email as a parameter and finally displays
-    the body of the response
+    Script that takes in a url and an email address, sends apost
+    request with the passed url with the email as a parameter and
+    finally displays the body of the response
 """
 
 if __name__ == "__main__":
     import requests, sys
-    data = {"email":sys.argv[2]}
+
+    data = {"email": sys.argv[2]}
     got = requests.post(sys.argv[1], data=data)
     print("{}".format(got.text))
